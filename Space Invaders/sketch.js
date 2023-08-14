0cd491a09b27a2e10889fae5b8c2ec73add18c81
Bapll <christofferssander@gmail.com>
2023-08-14 14:42:43 +0200

    Created Project
    8hxdtunhsxreunhsxer

---
+++ Space Invaders/sketch.js
@@ -0,0 +1,15 @@
+let w
+let wSize = 500
+let h
+let hSize = 500
+
+function setup() {
+  createCanvas(wSize, hSize);
+  background(255);
+}
+
+function draw() {
+  //console.log("test")
+  //w++; h++;
+  //resizeCanvas(sin(w)*wSize, cos(h)*hSize);
+}
